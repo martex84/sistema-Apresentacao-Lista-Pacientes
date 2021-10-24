cb5c214fb9513a60613fa117a941131a2fc3573d
martex84 <martex849@gmail.com>
2021-10-24 02:41:10 -0300

    Inclusao Search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { VscSearch } from 'react-icons/vsc'
 
 import { Header } from './components/header';
 import { Footer } from './components/footer';
@@ -7,10 +8,21 @@ import styles from './styles/App.module.scss'
 function App() {
 
   return (
-    <div className={styles.app}>
+    <div className={`${styles.app} d-flex flex-column align-items-stretch`}>
       <Header />
-      <section>
-
+      <section className={`${styles.sectionContainer} mb-auto`}>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus mauris elit sem convallis tincidunt eget. Nunc consequat et amet, sed ut. Mattis a, ut interdum phasellus ipsum, dictum dolor urna. Pulvinar turpis pharetra, dui id mi morbi enim semper sit.
+        </div>
+        <div className={`${styles.searchContainer} d-flex justify-content-center p-4`}>
+          <input type="text" placeholder="Searching" />
+          <button>
+            <VscSearch />
+          </button>
+        </div>
+        <div>
+          Tabela
+        </div>
       </section>
       <Footer />
     </div>
