70e8922f65e0a9257257a37e7f474978e6048ca5
martex84 <martex849@gmail.com>
2021-10-24 22:55:05 -0300

    Inclusao Row e Button More Row

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { VscSearch } from 'react-icons/vsc'
+import { BsSortAlphaDown, BsArrowClockwise } from 'react-icons/bs'
 
 import { Header } from './components/header';
 import { Footer } from './components/footer';
+import { Row } from './components/row';
 import styles from './styles/App.module.scss'
 
+
+type IDadosPessoa = {
+  nome: string,
+  email: string,
+  genero: string,
+  dataNascimento: string,
+  telefone: string,
+  nacionalidade: string,
+  endereco: string,
+  id: string,
+  url: string
+}
+
 function App() {
+
+  const [arrayPessoa, setArrayPessoa] = useState<IDadosPessoa[]>([]);
+
+  useEffect(() => {
+    if (arrayPessoa.length === 0) {
+      setArrayPessoa([{
+        nome: "Marcelo",
+        email: "Email",
+        genero: "Masculino",
+        dataNascimento: "00/00/00",
+        telefone: "Telefone",
+        nacionalidade: "Brasileiro",
+        endereco: "Endereço",
+        id: "asdasda",
+        url: "www.local.com.br"
+      }])
+    }
+  }, [])
 
   return (
     <div className={`${styles.app} d-flex flex-column align-items-stretch`}>
@@ -21,7 +54,56 @@ function App() {
           </button>
         </div>
         <div>
-          Tabela
+          <ul className={`${styles.containerTable}`}>
+            <ul className={`d-flex flex-row align-items-center justify-content-center`}>
+              <li className={`d-flex flex-row align-items-center justify-content-center`}>
+                <span>
+                  Name
+                </span>
+                <span className={styles.containerIcon}>
+                  <BsSortAlphaDown />
+                </span>
+              </li>
+              <li className={`d-flex flex-row align-items-center justify-content-center`}>
+                <span>
+                  Gender
+                </span>
+              </li>
+              <li className={`d-flex flex-row align-items-center justify-content-center`}>
+                <span>
+                  Birth
+                </span>
+              </li>
+              <li className={`d-flex flex-row align-items-center justify-content-center`}>
+                <span>
+                  Actions
+                </span>
+              </li>
+            </ul>
+            {arrayPessoa.map(valor => {
+              if (valor) {
+                return (
+                  <Row
+                    key={valor.id}
+                    nome={valor.nome}
+                    dataNascimento={valor.dataNascimento}
+                    genero={valor.genero}
+                    id={valor.id}
+                  />
+                );
+              }
+            })}
+          </ul>
+        </div>
+        <div className={`${styles.containerMoreRow} d-flex flex-row align-items-center justify-content-center mt-4`} >
+          <button>
+            <span>
+              <BsArrowClockwise size="30" />
+            </span>
+            <span>
+              Loading more ...
+            </span>
+          </button>
         </div>
       </section>
       <Footer />
--- frontend/src/components/row/index.tsx
+++ frontend/src/components/row/index.tsx
@@ -1,18 +1,30 @@
 
-
-interface PropsPessoa {
+import styles from './styles.module.scss';
+interface IPropsPessoa {
+    id: string
     nome: string,
-    email: string,
+    dataNascimento: string,
     genero: string,
 }
 
-export function Row(props: PropsPessoa) {
+export function Row(props: IPropsPessoa) {
     return (
         <>
             <li>
-                <ul>
-                    <li>
+                <ul className={`${styles.rowContainer} d-flex flex-row align-items-center justify-content-center`}>
+                    <li className={`d-flex flex-row align-items-center justify-content-center`}>
                         {props.nome}
+                    </li>
+                    <li className={`d-flex flex-row align-items-center justify-content-center`}>
+                        {props.genero}
+                    </li>
+                    <li className={`d-flex flex-row align-items-center justify-content-center`}>
+                        {props.dataNascimento}
+                    </li>
+                    <li className={`d-flex flex-row align-items-center justify-content-center`}>
+                        <button className={`btn btn-light`}>
+                            View
+                        </button>
                     </li>
                 </ul>
             </li>
