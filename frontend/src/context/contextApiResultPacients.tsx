b015c86a9b2f33cbc4baef6579e981b249ed71e8
martex84 <martex849@gmail.com>
2021-10-25 19:07:02 -0300

    Ateracao Data

--- frontend/src/context/contextApiResultPacients.tsx
+++ frontend/src/context/contextApiResultPacients.tsx
@@ -1,7 +1,7 @@
 import { Children, createContext, ReactNode, useEffect, useState } from "react";
 
 import { api } from "../services/api";
-import { DadosPessoaContext } from '../types'
+import { DadosPessoaContext, DadosPessoaContextFinal } from '../types'
 
 type ReturnPessoaGet = {
     data: any;
@@ -14,25 +14,54 @@ type ResultPessoaProvider = {
 
 type ResultPessoaContext = {
     changePage: (page: string) => Promise<void>,
-    user: DadosPessoaContext[];
+    user: DadosPessoaContextFinal[];
 }
 
 export const ResultContext = createContext({} as ResultPessoaContext);
 
 export function ResultPessoaProvider(props: ResultPessoaProvider) {
 
-    const [user, setUser] = useState<DadosPessoaContext[]>([]);
+    const [user, setUser] = useState<DadosPessoaContextFinal[]>([]);
 
     async function changePage(page: string) {
         const getPessoa = await api.get<ReturnPessoaGet>(`/?page=${page}&results=10&seed=abc&exc=login,registered,phone`);
 
-        const returno: DadosPessoaContext[] = [];
+        const returno: DadosPessoaContextFinal[] = [];
 
         getPessoa.data.results.map((value) => {
 
             if (value.id.value !== null) {
-                returno.push(value);
+
+                let returnFinal: any = {};
+
+                Object.entries(value).forEach(objeto => {
+                    if (objeto[0] === "dob") {
+
+                        const { date } = objeto[1] as any;
+
+                        const datafinal = new Date(date);
+
+                        let day = datafinal.getUTCDate().toString();
+
+                        let month = (datafinal.getUTCMonth() + 1).toString();
+
+                        returnFinal[objeto[0]] = {
+                            date: {
+                                day: day.length === 1 ? `0${day}` : day,
+                                month: month.length === 1 ? `0${month}` : month,
+                                year: datafinal.getUTCFullYear().toString()
+                            }
+                        }
+
+                    }
+                    else {
+                        returnFinal[objeto[0]] = objeto[1]
+                    }
+                })
+
+                returno.push(returnFinal);
             }
+
         })
 
         setUser(returno);
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -17,3 +17,27 @@ export type DadosPessoaContext = {
     },
     url: string
 }
+
+export type DadosPessoaContextFinal = {
+    name: {
+        title: string,
+        first: string,
+        last: string
+    },
+    email: string,
+    gender: string,
+    dob: {
+        date: {
+            day: string,
+            month: string,
+            year: string
+        }
+    },
+    cell: string,
+    nat: string,
+    location: string,
+    id: {
+        value: string
+    },
+    url: string
+}
